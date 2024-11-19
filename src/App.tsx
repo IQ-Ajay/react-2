fa4016c9afda85db240aad411c04b75fdf794dec
IQ-Ajay <ajay@indoqubix.com>
2024-11-19 09:10:50 +0530

    removed icons

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,6 @@ function App() {
   return (
     <>
     <MainComponent></MainComponent>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Nest</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
